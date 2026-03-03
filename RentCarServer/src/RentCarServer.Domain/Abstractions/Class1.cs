21840f297c47ec40c496005f21d60bd4647bb157
Umut <umut.dev@example.com>
2026-03-03 09:10:21 +0300

    Domain Katmanını tasarlayalım. Warningleri hataya dönüştürdüm

---
+++ RentCarServer/src/RentCarServer.Domain/Abstractions/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentCarServer.Domain.Abstractions;
+
+internal class Class1
+{
+}
--- RentCarServer/src/RentCarServer.Domain/Abstractions/Entiy.cs
+++ RentCarServer/src/RentCarServer.Domain/Abstractions/Entiy.cs
@@ -2,9 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RentCarServer.Domain.Abstractions
+namespace RentCarServer.Domain.Abstractions;
+
+public abstract class Entiy
 {
-    internal class Entiy
+    protected Entiy()
     {
+        Id = new IdentityId(Guid.CreateVersion7());
+        IsActive = true;
     }
+    public IdentityId Id { get;private set; }
+    public bool IsActive { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+    public IdentityId CreatedBy { get; private set; } =default!;
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    public IdentityId? UpdatedBy { get; private set; }
+    public bool IsDeleted { get; private set; }
+    public DateTimeOffset? DeletedAt { get; private set; }
+    public IdentityId? DeletedBy { get; private set; }
+
+    public void SetStatus(bool isActive)
+    {
+        IsActive = isActive;
+    }
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+}
+public sealed record IdentityId (Guid Value)
+{
+    public static implicit operator Guid(IdentityId id) => id.Value;
+    public static implicit operator string(IdentityId id) => id.Value.ToString();
 }
