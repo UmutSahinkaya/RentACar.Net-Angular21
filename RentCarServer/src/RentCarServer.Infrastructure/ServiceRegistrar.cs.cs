dc8b58653a52afda349e1561a619ae5f22c39d54
Umut <umut.dev@example.com>
2026-03-03 10:24:59 +0300

    Infrastructure Katmanını oluşturdum gerekli kütüphaneleri
    Add Infrastructure project with EF Core and DI setup
    
    Added RentCarServer.Infrastructure project to solution. Implemented ApplicationDbContext with global query filters and audit field management. Introduced extension methods for model configuration and DI registration. Added UserContext service for user ID retrieval from HTTP context. Included Scrutor for assembly scanning and configured EF Core conventions and value converters.

---
+++ RentCarServer/src/RentCarServer.Infrastructure/ServiceRegistrar.cs.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentCarServer.Infrastructure.Context;
+using Scrutor;
+
+namespace RentCarServer.Infrastructure;
+
+public static class ServiceRegistrar
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        _ = services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+        _ = services.Scan(scan => scan
+            .FromAssemblies(typeof(ServiceRegistrar).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        return services;
+    }
+}
---
+++ RentCarServer/src/RentCarServer.Infrastructure/Services/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using RentCarServer.Application.Services;
+using System.Security.Claims;
+
+namespace RentCarServer.Infrastructure.Services;
+
+internal sealed class UserContext(HttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public Guid GetUserId()
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        var claims = httpContext.User.Claims;
+        string? userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if (userId is null)
+            throw new ArgumentException("Kullanıcı bilgisi bulunamadı");
+        try
+        {
+            Guid id = Guid.Parse(userId);
+            return id;
+        }
+        catch (Exception)
+        {
+            throw new ArgumentException("Kullanıcı ID uygun guid formatında değil");
+        }
+
+    }
+}
