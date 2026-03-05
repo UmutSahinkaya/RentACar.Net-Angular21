3b041b42bf3e0bb81490aba6a9c766b5e3cfd85b
Umut <umut.dev@example.com>
2026-03-05 09:13:17 +0300

    Angular da login işlemi yapalım
    Implement login functionality with form validation and HTTP request handling

--- RentCarClient/apps/admin/src/interceptors/http-interceptor.ts
+++ RentCarClient/apps/admin/src/interceptors/http-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const url=req.url;
-  const endpoint = 'https://localhost:7207/';
+  const endpoint = 'https://localhost:7158/';
   const clone=req.clone({
     url: url.replace("/rent/",endpoint)
   });
--- RentCarClient/apps/admin/src/pages/login/login.ts
+++ RentCarClient/apps/admin/src/pages/login/login.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+/* eslint-disable no-debugger */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { HttpClient } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Result } from '../../models/result.model';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Login {
+  readonly #http = inject(HttpClient);
+  readonly #router = inject(Router);
 
+  login(form: NgForm) {
+    if (!form.valid) return;
+    
+    this.#http.post<Result<string>>('/rent/Auth/login', form.value).subscribe((res) => {
+        localStorage.setItem('response', res.data!);
+        this.#router.navigateByUrl("/");
+    });
+  }
 }
