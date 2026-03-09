119a9f80981e85e0decd6a1437b212cecaf47838
Umut <umut.dev@example.com>
2026-03-09 18:43:54 +0300

    BranchDeleteCommand ve BranchGetQuery istekleri oluşturuldu.(Add branch delete and get command/query handlers)
    Added BranchDeleteCommand and BranchGetQuery with their respective handlers for branch management. Handlers use dependency injection and TS.Result for standardized responses. Deletion and retrieval operations now return success or failure messages based on branch existence.

---
+++ RentCarServer/src/RentCarServer.Application/Branches/BranchGetQuery.cs
@@ -0,0 +1,19 @@
+﻿using RentCarServer.Domain.Branches;
+using RentCarServer.Domain.Branchs;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Branches;
+
+public sealed record BranchGetQuery(Guid Id) : IRequest<Result<Branch>>;
+
+internal sealed class BranchGetQueryHandler(IBranchRepository branchRepository) : IRequestHandler<BranchGetQuery, Result<Branch>>
+{
+    public async Task<Result<Branch>> Handle(BranchGetQuery request, CancellationToken cancellationToken)
+    {
+        var branch = await branchRepository.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        if (branch is null)
+            return Result<Branch>.Failure("Şube bulunamadı .");
+        return Result<Branch>.Succeed(branch);
+    }
+}
