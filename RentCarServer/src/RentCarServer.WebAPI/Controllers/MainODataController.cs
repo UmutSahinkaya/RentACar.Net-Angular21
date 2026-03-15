2be986903aec7601fd0013964303b984f8c7a3d0
Umut <umut.dev@example.com>
2026-03-15 11:38:47 +0300

    .NET de Role GetAll için OData yapısına uygun Controller endpoint yazalım(Add OData endpoint for roles and update EDM model)
    Added RoleDto entity set to EDM model and implemented /odata/roles GET endpoint to retrieve roles using RoleGetAllQuery. Imported RentCarServer.Application.Roles for support.

--- RentCarServer/src/RentCarServer.WebAPI/Controllers/MainODataController.cs
+++ RentCarServer/src/RentCarServer.WebAPI/Controllers/MainODataController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.OData.Routing.Controllers;
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
 using RentCarServer.Application.Branches;
+using RentCarServer.Application.Roles;
 using TS.MediatR;
 
 namespace RentCarServer.WebAPI.Controllers;
@@ -18,10 +19,15 @@ public class MainODataController : ODataController
         ODataConventionModelBuilder builder = new();
         _ = builder.EnableLowerCamelCase();
         _ = builder.EntitySet<BranchDto>("branches");
+        _ = builder.EntitySet<RoleDto>("roles");
         return builder.GetEdmModel();
     }
 
     [HttpGet("branches")]
     public async Task<IQueryable<BranchDto>> Branches(ISender sender, CancellationToken cancellationToken = default)
         => await sender.Send(new BranchGetAllQuery(), cancellationToken);
+
+    [HttpGet("roles")]
+    public async Task<IQueryable<RoleDto>> Roles(ISender sender, CancellationToken cancellationToken = default)
+        => await sender.Send(new RoleGetAllQuery(), cancellationToken);
 }
