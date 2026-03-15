dbb2ce9037feae38911ae0ca7119c04bc51acd99
Umut <umut.dev@example.com>
2026-03-15 09:57:59 +0300

    NET de Role Delete metodu yazalım(Add RoleDeleteCommand and handler for role deletion)
    Added RoleDeleteCommand to encapsulate role ID and implemented handler to check existence, mark as deleted, update repository, and save changes. Utilized dependency injection for repository and unit of work.

---
+++ RentCarServer/src/RentCarServer.Application/Roles/RoleDeleteCommand.cs
@@ -0,0 +1,23 @@
+﻿using GenericRepository;
+using RentCarServer.Domain.Roles;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Roles;
+
+public sealed record RoleDeleteCommand(Guid Id) : IRequest<Result<string>>;
+
+internal sealed class RoleDeleteCommandHandler(IRoleRepository roleRepository, IUnitOfWork unitOfWork) : IRequestHandler<RoleDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RoleDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var role = await roleRepository.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        if (role is null) return Result<string>.Failure("Silineccek Rol bulunamadı.");
+
+        role.Delete();
+        roleRepository.Update(role);
+        _ = await unitOfWork.SaveChangesAsync();
+
+        return "Role Başarıyla Silindi.";
+    }
+}
